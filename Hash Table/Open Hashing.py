bb78885a47cf6affd136c017333040bb5a30f8c3
SirinSky <hanvit0510@naver.com>
2021-09-20 00:01:56 +0900

    Update Open Hashing.py

--- Hash Table/Open Hashing.py
+++ Hash Table/Open Hashing.py
@@ -1,80 +1,74 @@
-# open hashing
-class OpenHash:
-    def __init__(self, table_size):
-        self.size = table_size
+class OpenHash():
+    def __init__(self, size):
+        self.size = size
         self.hash_table = [0 for a in range(self.size)]
         
-    def getKey(self, data):
-        self.key = ord(data[0])
-        return self.key
-    
-    def hashFunction(self, key):
-        return key % self.size
-
-    def getAddress(self, key):
-        myKey = self.getKey(key)
-        hash_address = self.hashFunction(myKey)
-        return hash_address
-    
-    def save(self, key, value):
-        hash_address = self.getAddress(key)
+    def hashing(self, key):
         
-        if self.hash_table[hash_address] != 0:
-            for a in range(len(self.hash_table[hash_address])):
-                if self.hash_table[hash_address][a][0] == key:
-                    self.hash_table[hash_address][a][1] = value
-                    return
-            self.hash_table[hash_address].append([key, value])
+        hash_address = (ord(key[0])) % self.size
+        return hash_address
+        
+    def save(self, key, value):
+        hash_address = self.hashing(key)
+        if(self.hash_table[hash_address] != 0):
+            for i in range(len(self.hash_table[hash_address])):
+                if(self.hash_table[hash_address][i][0] == key):
+                    self.hash_table[hash_address][i][1] = value
+                    print("저장됨(이미 저장되어 있어 값 갱신)")
+                    return True
+
+            (self.hash_table[hash_address]).append([key, value])
+            print("저장됨(이미 찬 공간에 중복 저장)")
+            return True
+                
         else:
             self.hash_table[hash_address] = [[key, value]]
-            
+            print("저장됨(빈 공간에 저장)")
+            return True
+        
     def read(self, key):
-        hash_address = self.getAddress(key)
+        hash_address = self.hashing(key)
         
-        if self.hash_table[hash_address] != 0:
-            for a in range(len(self.hash_table[hash_address])):
-                if self.hash_table[hash_address][a][0] == key:
-                    return self.hash_table[hash_address][a][0]
+        if(self.hash_table[hash_address] != 0):
+            for i in range (len(self.hash_table[hash_address])):
+                if(self.hash_table[hash_address][i][0] == key):
+                    value = self.hash_table[hash_address][i][1]
+                    print(str(key) + ", " + str(value))
+                    return True
+                    
+            print("해당 값 없음")
             return False
         else:
+            print("해당 값 없음")
             return False
-    
+        
     def delete(self, key):
-        hash_address = self.getAddress(key)
+        hash_address = self.hashing(key)
         
-        if self.hash_table[hash_address] != 0:
-            for a in range(len(self.hash_table[hash_address])):
-                if self.hash_table[hash_address][a][0] == key:
-                    if len(self.hash_table[hash_address]) == 1:
+        if(self.hash_table[hash_address] != 0):
+            for i in range(len(self.hash_table[hash_address])):
+                if (self.hash_table[hash_address][i][0] == key):
+                    if(len(self.hash_table[hash_address]) == 1):
+                        print("삭제 완료 [" + str(key) + ", " + str(self.hash_table[hash_address][i][1]) + "]")
                         self.hash_table[hash_address] = 0
+                        return True
                     else:
-                        del self.hash_table[hash_address][a]
-                    return
-            return False
+                        print("삭제 완료 [" + str(key) + ", " + str(self.hash_table[hash_address][i][1]) + "]")
+                        del (self.hash_table[hash_address][i])
+                        return True
+                    
         else:
+            print("삭제 불가(찾을 수 없음)")
             return False
-            
-            
-#Test Code
-h_table = OpenHash(8)
 
-h_table.save('aa', '1111')
-h_table.read('aa')
 
-data1 = 'aa'
-data2 = 'ad'
+a = OpenHash(10)
 
-print(ord(data1[0]))
-print(ord(data2[0]))
+a.save('sirin', 'sky')               #저장됨(빈 공간에 저장)
+a.save('serim', 'bradely')           #저장됨(이미 찬 공간에 중복 저장)
+a.save('sirin', 'sky\'s library')    #저장됨(이미 저장되어 있어 값 갱신)
+a.read('sirin')                      #sirin, sky's library
 
-h_table.save('ad', '2222')
-print(h_table.hash_table)
-
-h_table.read('aa')
-h_table.read('ad')
-
-h_table.delete('aa')
-print(h_table.hash_table)
-print(h_table.delete('Data'))
-h_table.delete('ad')
-print(h_table.hash_table)
+a.delete('sirin')                    #해당 값 없음
+a.read('sirin')                     #해당 값 없음
+a.read('serim')
